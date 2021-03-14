9f62d167ed52fea92f1fd64996c48e0234af07fa
Bagus wahit bayunara <80232218+BaguswahitB@users.noreply.github.com>
2021-03-14 19:46:58 +0700

    Add files via upload

---
+++ Pertemuan 1/Praktek/TestTV.java
@@ -0,0 +1,19 @@
+package Praktek;
+
+public class TestTV {
+    public static void main(String[] args) {
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(121);
+        tv1.setVolume(10);
+        
+        TV tv2 = new TV();
+        tv2.turnOn();
+        tv2.channelUP();
+        tv2.channelUP();
+        tv2.volumeUP();
+        
+        System.out.println(" tv1's channel is " + tv1.channel + " and volume level is " + tv1.volumeLevel);
+        System.out.println(" tv2's channel is " + tv2.channel + " and volume level is " + tv2.volumeLevel);
+    }
+}
